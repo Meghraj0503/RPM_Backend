091ac355b5c19ad27239711bc29da97ae24a1c1a
Meghraj0503 <meghraj.g@humingtec.com>
2026-05-11 12:49:12 +0530

    training Token

--- training-service/authMiddleware.js
+++ training-service/authMiddleware.js
@@ -1,15 +1,16 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-    const authHeader = req.headers['authorization'];
-    if (!authHeader) return res.status(401).json({ error: 'Access denied' });
+    const authHeader = req.headers.authorization;
+    console.log(authHeader)
+    if (!authHeader || !authHeader.startsWith('Bearer ')) return res.status(401).json({ error: 'Unauthorized: Missing or invalid token' });
+
     const token = authHeader.split(' ')[1];
-    if (!token) return res.status(401).json({ error: 'Access denied' });
     try {
-        const verified = jwt.verify(token, process.env.JWT_SECRET || 'supersecretjwtkey_for_rpm');
-        req.user = verified;
+        const decoded = jwt.verify(token, process.env.JWT_SECRET || 'fallback_secret');
+        req.user = decoded;
         next();
-    } catch (err) {
-        res.status(400).json({ error: 'Invalid token' });
+    } catch (error) {
+        return res.status(401).json({ error: 'Unauthorized: Invalid token' });
     }
 };
